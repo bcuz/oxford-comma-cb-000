07dbde8c949d4b3ee2b14a95fcbce83cd7fcf077
adam n <adam.nawroc@gmail.com>
2018-02-28 16:18:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,15 +3,15 @@ def oxford_comma(array)
   string = ''
   array.each_with_index do |item, i|
 
-  if array.size == 1
-    string += item
-  elsif array.size == 2 and i + 1 != array.size
-    string += "#{item} "
-  elsif i + 1 == array.size
-    string += "and #{item}"
-  else
-    string += "#{item}, "
-  end
+    if array.size == 1
+      string += item
+    elsif array.size == 2 and i + 1 != array.size
+      string += "#{item} "
+    elsif i + 1 == array.size
+      string += "and #{item}"
+    else
+      string += "#{item}, "
+    end
 
   end
 
